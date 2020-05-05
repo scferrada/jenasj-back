fcbc1acb0db0450ebb26fb6da27f85a0e3a047f9
scferrada <scferradaa@gmail.com>
2020-05-04 20:35:26 -0400

    spring-boot skeleton and api for query execution

---
+++ src/main/java/com/apache/jena/sj/sjendpoint/SjEndpointApplication.java
@@ -0,0 +1,13 @@
+package com.apache.jena.sj.sjendpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SjEndpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SjEndpointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apache/jena/sj/sjendpoint/SjEndpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apache.jena.sj.sjendpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SjEndpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
